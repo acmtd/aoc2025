eeca061a738cc680820e546abc639d0e4c91c5c4
Andy Cross <across@dextersystems.com>
2025-12-02 15:50:52 -0500

    add day 3 template

---
+++ src/main/kotlin/day3/Solution.kt
@@ -0,0 +1,23 @@
+package day3
+
+import println
+import readAsString
+
+fun main() {
+    val testInput = readAsString("day3/test")
+    val puzzleInput = readAsString("day3/problem")
+
+    check(part1(testInput) == 0L)
+    part1(puzzleInput).println()
+
+    check(part2(testInput) == 0L)
+    part2(puzzleInput).println()
+}
+
+fun part1(input: String): Long {
+    throw NotImplementedError()
+}
+
+fun part2(input: String): Long {
+    throw NotImplementedError()
+}
