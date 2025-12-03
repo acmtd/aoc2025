65516ff7875dcb201b4929450ff5ac392a004dfa
Andy Cross <across@dextersystems.com>
2025-12-03 08:28:21 -0500

    Day 3 solution

--- src/main/kotlin/day3/Solution.kt
+++ src/main/kotlin/day3/Solution.kt
@@ -1,23 +1,55 @@
 package day3
 
 import println
-import readAsString
+import readAsLines
 
 fun main() {
-    val testInput = readAsString("day3/test")
-    val puzzleInput = readAsString("day3/problem")
+    val testInput = readAsLines("day3/test")
+    val puzzleInput = readAsLines("day3/problem")
 
-    check(part1(testInput) == 0L)
+    check(part1(testInput) == 357)
     part1(puzzleInput).println()
 
-    check(part2(testInput) == 0L)
+    check(part2(testInput) == 3121910778619L)
     part2(puzzleInput).println()
 }
 
-fun part1(input: String): Long {
-    throw NotImplementedError()
+fun part1(banks: List<String>): Int {
+    return banks.sumOf { it.highestJoltage() }
 }
 
-fun part2(input: String): Long {
-    throw NotImplementedError()
+fun part2(banks: List<String>): Long {
+    return banks.sumOf { it.highestTwelveBatteryJoltage() }
+}
+
+private fun String.highestJoltage(): Int {
+    val batteries = this.chunked(1).map { it.toInt() }
+
+    val first = batteries.dropLast(1).max()
+    val firstPos = batteries.indexOfFirst { it == first }
+
+    val last = batteries.drop(firstPos + 1).max()
+
+    return first * 10 + last
+}
+
+private fun String.highestTwelveBatteryJoltage(): Long {
+    var batteries = this.chunked(1).map { it.toInt() }
+
+    val maxDigits = 12
+    var digitsRemaining = maxDigits
+
+    val chosenBatteries = mutableListOf<Int>()
+
+    while (digitsRemaining > 0) {
+        val highest = batteries.dropLast(digitsRemaining - 1).max()
+        val highestPos = batteries.indexOfFirst { it == highest }
+
+        batteries = batteries.drop(highestPos + 1)
+
+        chosenBatteries.add(highest)
+        digitsRemaining--
+    }
+
+    return chosenBatteries.joinToString("") { it.toString() }.toLong()
 }
