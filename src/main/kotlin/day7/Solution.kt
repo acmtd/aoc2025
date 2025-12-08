f45ceffda58396cb1a06b53d34d91fce58c9f762
Andy Cross <across@dextersystems.com>
2025-12-07 20:34:14 -0500

    Day 7 part 2 solution, ugly but works

--- src/main/kotlin/day7/Solution.kt
+++ src/main/kotlin/day7/Solution.kt
@@ -2,6 +2,8 @@ package day7
 
 import println
 import readAsLines
+import kotlin.collections.emptyList
+import kotlin.collections.forEach
 
 fun main() {
     val testInput = readAsLines("day7/test")
@@ -9,6 +11,9 @@ fun main() {
 
     val puzzleInput = readAsLines("day7/problem")
     part1(puzzleInput).println()
+
+    check(part2(testInput) == 40L)
+    part2(puzzleInput).println() // 923423101 is too low
 }
 
 data class Point(val row: Int, val col: Int)
@@ -37,8 +42,62 @@ fun part1(lines: List<String>): Int {
     return visitedSplitters.size
 }
 
+data class SplitterState(val nextSplitters: Set<Point>, val timelines: Long)
+
+fun part2(lines: List<String>): Long {
+    val state = parse(lines)
+
+    val splitterStates = state.splitters.associateWith { SplitterState(emptySet(), 0L) }.toMutableMap()
+
+    state.splitters.forEach { splitter ->
+        var timelines = 0L
+        val nextSplitters = mutableSetOf<Point>()
+
+        splitter.adjacentPoints().forEach { p ->
+            val next = nextSplitter(p, state.splitters)
+
+            if (next == null) {
+                timelines += 1
+            } else {
+                nextSplitters.add(next)
+            }
+        }
+
+        splitterStates[splitter] = SplitterState(nextSplitters, timelines)
+    }
+
+    while (true) {
+        val solvedSplitters = splitterStates.filter { it.value.nextSplitters.isEmpty() }.keys
+
+        if (solvedSplitters.size == splitterStates.size) {
+            return splitterStates.minBy { it.key.row }.value.timelines
+        }
+
+        solvedSplitters.forEach { splitter ->
+            val unsolvedSplitters = splitterStates.filter { splitter in it.value.nextSplitters }.keys
+
+            unsolvedSplitters.forEach { unsolved ->
+                val existingState = splitterStates[unsolved]!!
+                val newState = SplitterState(
+                    existingState.nextSplitters - splitter,
+                    existingState.timelines + splitterStates[splitter]!!.timelines
+                )
+                splitterStates[unsolved] = newState
+            }
+        }
+    }
+
+}
+
 fun nextSplitter(point: Point, splitters: List<Point>): Point? {
     return splitters.filter { it.col == point.col && it.row > point.row }.minByOrNull { it.row }
+}
+
+fun Point.adjacentPoints(): Set<Point> {
+    return setOf(
+        Point(row, col - 1),
+        Point(row, col + 1)
+    )
 }
 
 fun parse(lines: List<String>): State {
